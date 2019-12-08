27085ff3c84bafc91ed5788f5dc0199491c9fc64
igor.m.peixoto <igor.m.peixoto@gmail.com>
2019-12-07 22:23:31 -0300

    estruturação do redux na aplicação

--- src/index.js
+++ src/index.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
+import { Provider } from 'react-redux';
 import App from './App';
+import configureStore from './store/configureStore';
+
+const store = configureStore();
+// store.dispatch();
 
 ReactDOM.render(
-  <Router>
-    <App />
-  </Router>,
+  <Provider store={store}>
+    <Router>
+      <App />
+    </Router>
+  </Provider>,
   document.getElementById('root')
 );
